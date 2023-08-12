9569c2d5b9d744484f6048a4240012cf85958737
SukhrobiddinUmarov <suhrob.u@gmail.com>
2023-08-12 23:51:47 +0500

    seminar tasks

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число 1!");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2!");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number2 * number2 == number1)
+    {Console.WriteLine($"Число {number1} является квадратом числа {number2}");}
+else 
+    {Console.WriteLine($"Число {number1} не вляется квадратом числа {number2}");}
+
---
+++ task7/Program.cs
@@ -0,0 +1,16 @@
+﻿void NumberEntry()
+    {
+        Console.WriteLine("Введите число!");
+        string entry = Console.ReadLine();
+        int number = Convert.ToInt32(entry);
+        if(number < 100 || number > 1000)
+            {
+                Console.WriteLine("Введенное число не трехзначное!");
+                NumberEntry();
+            }
+        int result = number % 10;
+        Console.WriteLine($"{result}");
+        
+}
+
+NumberEntry();
