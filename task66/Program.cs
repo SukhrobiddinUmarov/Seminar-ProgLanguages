7d78bfa9dbc792f4d2c7749f735884dc5d38675a
SukhrobiddinUmarov <suhrob.u@gmail.com>
2023-08-26 00:37:59 +0500

    interim tasks

---
+++ task66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+System.Console.Write("Введите начальное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите конечное число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int sum = number1;
+
+for (int i = number1+1; i <= number2; i++)
+{
+    sum += i;
+}
+System.Console.WriteLine(sum);
---
+++ task68/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+System.Console.Write("Введите значение m: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите значение n: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int AckermannFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n +1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return AckermannFunction(m-1, 1);
+    }
+    return AckermannFunction(m-1, AckermannFunction(m, n-1));
+}
+
+System.Console.WriteLine(AckermannFunction(number1, number2));
